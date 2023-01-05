8443f58534fcc8cc8d30b8fb1ba7e7f42d2e90b3
laurszalk <laurenszalk@gmail.com>
2023-01-05 13:16:39 -0500

    updated local storage function

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,16 @@
 var startButtonEl = document.getElementById("btn-start");
-// var firstButtonEl = document.getElementById("btn-first");
-// var secondButtonEl = document.getElementById("btn-second");
-// var thirdButtonEl = document.getElementById("btn-third");
-// var fourthButtonEl = document.getElementById("btn-fourth");
 var timerEl = document.getElementById("timer");
-var scoreEl = document.getElementById("score");
 var hiddenQuestionEl = document.getElementById("hidden");
+var userInputBox = document.getElementById("user-input-box");
+var userInputName = document.getElementById("name");
 var questionText = document.querySelector(".question-text");
 var cardBody = document.querySelector("#card-body");
 var currentQuestion = 0; //because our array starts at 0
 var currentAnswer = ""; //our answer is a string
-var secondsLeft = 0;
+var secondsLeft;
+var quizTimer;
+var formEl = document.getElementById("form");
+var listHighScores = [];
 
 var questions = [
   {
@@ -57,26 +57,55 @@ var questions = [
 ];
 
 startButtonEl.addEventListener("click", function (event) {
-  var secondsLeft = 60;
-  var quizTimer = setInterval(function () {
+  //click the button and start timer
+  startButtonEl.style.display = "none";
+  secondsLeft = 60;
+  quizTimer = setInterval(function () {
     secondsLeft--;
     if (secondsLeft > 0) {
       timerEl.textContent = "Time: " + secondsLeft;
     }
 
-    if (secondsLeft === 0 || currentQuestion === questions.length) {
+    if (secondsLeft === 0) {
       clearInterval(quizTimer); //quiz ends
-      //call the function or unhide box
+      endGame();
+      //hiddenQuestionEl.style.display = "none";
+      // userInputBox.style.display = "block"; //unhide box
+      // alert("Game Over. Your score is " + secondsLeft + ".");
     }
   }, 1000);
   hiddenQuestionEl.style.display = "block";
   newQuestions();
 });
-//need a function to store and retrieve from local storage
-//store secondsLeft (time remaining variable)
-//add a hidden div to put a text box for initials input
-//add an input field and button to call the function to store to local storage
-//don't forget json.stringify, json.parse
+function endGame() {
+  hiddenQuestionEl.style.display = "none";
+  userInputBox.style.display = "block"; //unhide box
+  alert("Game Over. Your score is " + secondsLeft + ".");
+}
+//var highScores = { name: "", score: secondsLeft };
+function showScore(event) {
+  event.preventDefault();
+  var highScores = { name: userInputName.value, score: secondsLeft };
+  document.querySelector("#score-box").textContent =
+    "Name: " + userInputName.value + " Score: " + secondsLeft;
+  console.log(highScores);
+  listHighScores.push(highScores);
+  localStorage.setItem("highscore", JSON.stringify(listHighScores)); //saving highscores to local storage
+
+  //   localStorage.getItem(highScores, JSON.parse(highScores));
+  //
+}
+
+formEl.addEventListener("submit", showScore);
+formEl.addEventListener("submit", function (event) {
+  startButtonEl.style.display = "block";
+  userInputBox.style.display = "none";
+});
+
+//add another event listner to unhide the start button
+//create another button to click to show high scores
+//populates list of scores to show to the user
+
 function newQuestions() {
   questionText.textContent = questions[currentQuestion].query;
   currentAnswer = questions[currentQuestion].correctAnswer; //store the answers so we can compare later
@@ -95,10 +124,14 @@ cardBody.addEventListener("click", function (event) {
   } else {
     alert("You got it wrong!");
     secondsLeft -= 5; //penalizes them 5 seconds if the answer is wrong
+    //currently not deducting I don't know why
   }
   currentQuestion++;
   if (currentQuestion !== questions.length) {
     newQuestions();
+  } else {
+    clearInterval(quizTimer);
+    endGame();
   }
 });
 
